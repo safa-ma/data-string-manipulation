8914b367f61ae41ecf1466559507a50d173630fe
safa albukhari <bukharisafam@gmail.com>
2025-09-01 15:59:17 +0300

    Progress on string_methods: XXX tests passing

--- string_methods.py
+++ string_methods.py
@@ -9,42 +9,42 @@ def add_comma(a_string):
     returns a copy of the string with every word separated by a comma
     example: add_comma("John Peter Jude") => "John, Peter, Jude"
     """
-    pass  # YOUR CODE HERE
+    return ", ".join(a_string. split())
 
 def belongs_to(a_string, a_word):
     """
     returns True if a_string contains a_word
     example: belongs_to("hey jude", "jude") => True
     """
-    pass  # YOUR CODE HERE
+    return a_word.lower() in a_string.lower()
 
 def count_repetition(a_string, a_substring):
     """
     returns how many times a_substring occurs in a_string
     example: count_repetition("000123000123", "0") => 6
     """
-    pass  # YOUR CODE HERE
+    return a_string.count(a_substring)
 
 def is_a_question(a_string):
     """
     returns True if a_string ends with a "?"
     example: is_a_question("How are you?") => True
     """
-    pass  # YOUR CODE HERE
+    return a_string.endswith("?")
 
 def remove_surrounding_whitespaces(a_string):
     """
     returns a copy of the string with leading and trailing whitespaces removed
     example: remove_surrounding_whitespaces("  hey yo  ") => "hey yo"
     """
-    pass  # YOUR CODE HERE
+    return a_string.strip()
 
 def replace(initial_string, old_letter, new_letter):
     """
     returns a copy of the string with the new letter replacing the old one
     example: replace("casanova", "a", "o") => "cosonovo"
     """
-    pass  # YOUR CODE HERE
+    return initial_string.replace(old_letter, new_letter)
 
 def full_description_concatenation(first_name, last_name, age):
     """
@@ -52,7 +52,7 @@ def full_description_concatenation(first_name, last_name, age):
      the age using concatenation
     example: full_description_concatenation("john", "doe", 33) => "John Doe is 33"
     """
-    pass  # YOUR CODE HERE
+    return first_name.capitalize() + " " + last_name.capitalize() + " is " + str(age)
 
 def full_description_formatting(first_name, last_name, age):
     """
@@ -60,4 +60,4 @@ def full_description_formatting(first_name, last_name, age):
      the age using string interpolation
     example: full_description_formatting("john", "doe", 33) => "John Doe is 33"
     """
-    pass  # YOUR CODE HERE
+    return f"{first_name.capitalize()} {last_name.capitalize()} is {age}"
